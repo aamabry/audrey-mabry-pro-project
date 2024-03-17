187f95053db3bc607c193d1ff35cc31d597e934a
flangoria <audrey.mabry90@gmail.com>
2024-03-17 09:38:22 -0400

    added humidity and wind speed js

--- src/index.js
+++ src/index.js
@@ -32,12 +32,16 @@ function showWeather(response) {
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#current-city");
   let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
   let currentDateELement = document.querySelector("#current-date");
   let currentDate = new Date();
   currentDateELement.innerHTML = formatDate(currentDate);
   temperatureElement.innerHTML = Math.round(temperature);
   descriptionElement.innerHTML = response.data.condition.description;
   cityElement.innerHTML = response.data.city;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
 
 }
 
