9bae59291a3c342bda444d08f3f75775e2c22e89
flangoria <audrey.mabry90@gmail.com>
2024-03-18 15:07:30 -0400

    edit css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 function showWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
-  let cityElement = document.querySelector("#current-city");
+  let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windSpeedElement = document.querySelector("#wind-speed");
@@ -18,8 +18,8 @@ function showWeather(response) {
   iconElement.innerHTML = `<img src= "${response.data.condition.icon_url}"class="current-temperature-icon"/>`;
 
   getForecast(response.data.city);
-
 }
+
 function formatDate(date) {
   let minutes = date.getMinutes();
   let hours = date.getHours();
@@ -32,18 +32,19 @@ function formatDate(date) {
     "Friday",
     "Saturday",
   ];
+
   let day = days[date.getDay()];
 
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
-
   if (hours < 10) {
     hours = `0${hours}`;
   }
-  
+
   return `${day} ${hours}:${minutes}`;
 }
+
 function searchCity(city) {
   let apiKey = "1b509431b344bbaa8c5fo44ef08bca6t";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
@@ -59,7 +60,7 @@ function handleSearchSubmit(event) {
 
 function formatDay(timestamp){
   let date = new Date(timestamp * 1000);
-  let days = ["Sun", "Mon", "Tues", "Wed", "Thu", "Fri", "Sat", "Sun"];
+  let days = ["Sun", "Mon", "Tues", "Wed", "Thu", "Fri", "Sat", "Sun",];
 
   return days[date.getDay()];
 }
@@ -71,28 +72,22 @@ function getForecast(city){
 }
 
 function displayForecast(response){
-  
   let forecastHtml = "";
   
-
   response.data.daily.forEach(function (day, index) {
     if (index < 5){
     forecastHtml =
       forecastHtml +
-
-      `
-      <div class="weather-forecast-day">
+      ` <div class="weather-forecast-day">
         <div class="weather-forecast-date">${formatDay(day.time)}</div>
-        <div class="weather-forecast-icon">
-        <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
+        <div class="weather-forecast-icon"><img src="${day.condition.icon_url}" class="weather-forecast-icon" />
         </div>
         <div class="weather-forecast-temperatures">
         <div class="weather-forecast-temperature">
           <strong>${Math.round(day.temperature.maximum)}°</strong>
          <div class="weather-forecast-temperature">${Math.round(day.temperature.minimum)}°</div>
          </div>
-         </div> 
-      `;
+         </div>`;
     }
   });
 
