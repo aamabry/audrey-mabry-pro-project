704c2cd8745b9d77abec3d53d0fc94151ef3b356
flangoria <audrey.mabry90@gmail.com>
2024-03-18 19:30:44 -0400

    fixed forecast elements

--- src/index.js
+++ src/index.js
@@ -79,15 +79,16 @@ function displayForecast(response){
     forecastHtml =
       forecastHtml +
       ` <div class="weather-forecast-day">
-        <div class="weather-forecast-date">${formatDay(day.time)}</div>
-        <div class="weather-forecast-icon"><img src="${day.condition.icon_url}" class="weather-forecast-icon" />
-        </div>
-        <div class="weather-forecast-temperatures">
-        <div class="weather-forecast-temperature">
+      <div class="weather-forecast-date">${formatDay(day.time)}</div>
+      <div class="weather-forecast-icon"><img src="${day.condition.icon_url}" class="weather-forecast-icon" />
+      </div>
+      <div class="weather-forecast-temperatures">
+        <div>
           <strong>${Math.round(day.temperature.maximum)}°</strong>
-         <div class="weather-forecast-temperature">${Math.round(day.temperature.minimum)}°</div>
-         </div>
-         </div>`;
+        </div>
+        <div class="weather-forecast-temperature">${Math.round(day.temperature.minimum)}°</div>
+      </div>
+    </div>`;
     }
   });
 
