d4c0b0cc0514ff9af8f56de99c79a2214eee51d4
Audrey Mabry <152105571+aamabry@users.noreply.github.com>
2024-07-07 18:37:07 +0000

    Moved code from sandbox to VS Code, inejcted npm react and axios

---
+++ dist/23231b6a8bf41d0161548fccbf3b8085.js
@@ -0,0 +1,281 @@
+// modules are defined as an array
+// [ module function, map of requires ]
+//
+// map of requires is short require name -> numeric require
+//
+// anything defined in a previous bundle is accessed via the
+// orig method which is the require for previous bundles
+
+// eslint-disable-next-line no-global-assign
+require = (function (modules, cache, entry) {
+  // Save the require from previous bundle to this closure if any
+  var previousRequire = typeof require === "function" && require;
+
+  function newRequire(name, jumped) {
+    if (!cache[name]) {
+      if (!modules[name]) {
+        // if we cannot find the module within our internal map or
+        // cache jump to the current global require ie. the last bundle
+        // that was added to the page.
+        var currentRequire = typeof require === "function" && require;
+        if (!jumped && currentRequire) {
+          return currentRequire(name, true);
+        }
+
+        // If there are other bundles on this page the require from the
+        // previous one is saved to 'previousRequire'. Repeat this as
+        // many times as there are bundles until the module is found or
+        // we exhaust the require chain.
+        if (previousRequire) {
+          return previousRequire(name, true);
+        }
+
+        var err = new Error('Cannot find module \'' + name + '\'');
+        err.code = 'MODULE_NOT_FOUND';
+        throw err;
+      }
+
+      localRequire.resolve = resolve;
+
+      var module = cache[name] = new newRequire.Module(name);
+
+      modules[name][0].call(module.exports, localRequire, module, module.exports);
+    }
+
+    return cache[name].exports;
+
+    function localRequire(x){
+      return newRequire(localRequire.resolve(x));
+    }
+
+    function resolve(x){
+      return modules[name][1][x] || x;
+    }
+  }
+
+  function Module(moduleName) {
+    this.id = moduleName;
+    this.bundle = newRequire;
+    this.exports = {};
+  }
+
+  newRequire.isParcelRequire = true;
+  newRequire.Module = Module;
+  newRequire.modules = modules;
+  newRequire.cache = cache;
+  newRequire.parent = previousRequire;
+
+  for (var i = 0; i < entry.length; i++) {
+    newRequire(entry[i]);
+  }
+
+  // Override the current require with this new one
+  return newRequire;
+})({6:[function(require,module,exports) {
+function showWeather(response) {
+  var temperatureElement = document.querySelector("#temperature");
+  var temperature = response.data.temperature.current;
+  var cityElement = document.querySelector("#city");
+  var descriptionElement = document.querySelector("#description");
+  var humidityElement = document.querySelector("#humidity");
+  var windSpeedElement = document.querySelector("#wind-speed");
+  var timeElement = document.querySelector("#time");
+  var date = new Date(response.data.time * 1000);
+  var iconElement = document.querySelector("#icon");
+
+  timeElement.innerHTML = formatDate(date);
+  temperatureElement.innerHTML = Math.round(temperature);
+  descriptionElement.innerHTML = response.data.condition.description;
+  cityElement.innerHTML = response.data.city;
+  humidityElement.innerHTML = response.data.temperature.humidity + "%";
+  windSpeedElement.innerHTML = response.data.wind.speed + "km/h";
+  iconElement.innerHTML = "<img src= \"" + response.data.condition.icon_url + "\"class=\"current-temperature-icon\"/>";
+
+  getForecast(response.data.city);
+}
+
+function formatDate(date) {
+  var minutes = date.getMinutes();
+  var hours = date.getHours();
+  var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+  var day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
+
+  return day + " " + hours + ":" + minutes;
+}
+
+function searchCity(city) {
+  var apiKey = "1b509431b344bbaa8c5fo44ef08bca6t";
+  var apiUrl = "https://api.shecodes.io/weather/v1/current?query=" + city + "&key=" + apiKey + "&units=imperial";
+  axios.get(apiUrl).then(showWeather);
+}
+
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  var searchInput = document.querySelector("#search-input");
+
+  searchCity(searchInput.value);
+}
+
+function formatDay(timestamp) {
+  var date = new Date(timestamp * 1000);
+  var days = ["Sun", "Mon", "Tues", "Wed", "Thu", "Fri", "Sat", "Sun"];
+
+  return days[date.getDay()];
+}
+
+function getForecast(city) {
+  var apiKey = "1b509431b344bbaa8c5fo44ef08bca6t";
+  var apiUrl = "https://api.shecodes.io/weather/v1/forecast?query=" + city + "&key=" + apiKey + "&units=imperial";
+  axios(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
+  var forecastHtml = "";
+
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml = forecastHtml + (" <div class=\"weather-forecast-day\">\n      <div class=\"weather-forecast-date\">" + formatDay(day.time) + "</div>\n      <div class=\"weather-forecast-icon\"><img src=\"" + day.condition.icon_url + "\" class=\"weather-forecast-icon\" />\n      </div>\n      <div class=\"weather-forecast-temperatures\">\n        <div>\n          <strong>" + Math.round(day.temperature.maximum) + "\xB0</strong>\n        </div>\n        <div class=\"weather-forecast-temperature\">" + Math.round(day.temperature.minimum) + "\xB0</div>\n      </div>\n    </div>");
+    }
+  });
+
+  var forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+var searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", handleSearchSubmit);
+
+searchCity("New York");
+displayForecast();
+},{}],13:[function(require,module,exports) {
+
+var global = (1, eval)('this');
+var OldModule = module.bundle.Module;
+function Module(moduleName) {
+  OldModule.call(this, moduleName);
+  this.hot = {
+    accept: function (fn) {
+      this._acceptCallback = fn || function () {};
+    },
+    dispose: function (fn) {
+      this._disposeCallback = fn;
+    }
+  };
+}
+
+module.bundle.Module = Module;
+
+var parent = module.bundle.parent;
+if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
+  var hostname = '' || location.hostname;
+  var protocol = window.location.protocol === 'https:' ? 'wss' : 'ws';
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '45197' + '/');
+  ws.onmessage = function (event) {
+    var data = JSON.parse(event.data);
+
+    if (data.type === 'update') {
+      data.assets.forEach(function (asset) {
+        hmrApply(global.require, asset);
+      });
+
+      data.assets.forEach(function (asset) {
+        if (!asset.isNew) {
+          hmrAccept(global.require, asset.id);
+        }
+      });
+    }
+
+    if (data.type === 'reload') {
+      ws.close();
+      ws.onclose = function () {
+        location.reload();
+      };
+    }
+
+    if (data.type === 'error-resolved') {
+      console.log('[parcel] ✨ Error resolved');
+    }
+
+    if (data.type === 'error') {
+      console.error('[parcel] 🚨  ' + data.error.message + '\n' + 'data.error.stack');
+    }
+  };
+}
+
+function getParents(bundle, id) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return [];
+  }
+
+  var parents = [];
+  var k, d, dep;
+
+  for (k in modules) {
+    for (d in modules[k][1]) {
+      dep = modules[k][1][d];
+      if (dep === id || Array.isArray(dep) && dep[dep.length - 1] === id) {
+        parents.push(+k);
+      }
+    }
+  }
+
+  if (bundle.parent) {
+    parents = parents.concat(getParents(bundle.parent, id));
+  }
+
+  return parents;
+}
+
+function hmrApply(bundle, asset) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return;
+  }
+
+  if (modules[asset.id] || !bundle.parent) {
+    var fn = new Function('require', 'module', 'exports', asset.generated.js);
+    asset.isNew = !modules[asset.id];
+    modules[asset.id] = [fn, asset.deps];
+  } else if (bundle.parent) {
+    hmrApply(bundle.parent, asset);
+  }
+}
+
+function hmrAccept(bundle, id) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return;
+  }
+
+  if (!modules[id] && bundle.parent) {
+    return hmrAccept(bundle.parent, id);
+  }
+
+  var cached = bundle.cache[id];
+  if (cached && cached.hot._disposeCallback) {
+    cached.hot._disposeCallback();
+  }
+
+  delete bundle.cache[id];
+  bundle(id);
+
+  cached = bundle.cache[id];
+  if (cached && cached.hot && cached.hot._acceptCallback) {
+    cached.hot._acceptCallback();
+    return true;
+  }
+
+  return getParents(global.require, id).some(function (id) {
+    return hmrAccept(global.require, id);
+  });
+}
+},{}]},{},[13,6])
+//# sourceMappingURL=/dist/23231b6a8bf41d0161548fccbf3b8085.map
